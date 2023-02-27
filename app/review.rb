5775685eea31ce8c9ae9e656d9fe6a59a0f50873
Maryann <86890821+mary-ruguru@users.noreply.github.com>
2023-02-27 09:52:02 +0300

    Update review.rb

--- app/review.rb
+++ app/review.rb
@@ -1,7 +1,17 @@
 class Review < ApplicationRecord::Base
+    
+          #Review
+    #Review#user
+     #Returns the User instance for this Review
+    #Review#product
+      #Returns the Product instance for this Review
 
     belongs_to :user
     belongs_to :product
+    
+    Review#print_review
+  #This should puts in the terminal a string formatted as follows: Review for {insert product name} by {insert user name}: {insert review star_rating}.    {insert review comment}
+
 
     def print_review
         return "Review for #{self.product.name} by #{self.user.name}: #{self.star_rating}. #{self.comment}"
