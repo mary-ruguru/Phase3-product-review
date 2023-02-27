4e48a975ae70a9908c88a521d424251fd872ea0d
Maryann <86890821+mary-ruguru@users.noreply.github.com>
2023-02-27 10:00:39 +0300

    Update 20230226120513_create_reviews.rb

--- db/migrate/20230226120513_create_reviews.rb
+++ db/migrate/20230226120513_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
+  
+  #A Review belongs to a Product, and a Review also belongs to a User. In your migration, create any columns your reviews table will need to establish these relationships.
+  #The reviews table should also have:
+  #A star_rating column that stores an integer.
+  #A comment column that stores a string.
   def change
     create_table :reviews do |t|
       t.integer :star_rating
@@ -11,3 +16,5 @@ class CreateReviews < ActiveRecord::Migration[7.0]
     end  
   end
 end
+
+#NB: my seeding did not work because of uninitialized constant on the product create_product.... but i checked how i had named my class and they were all correct
